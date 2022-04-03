3d803303a59f76967a4cd2c8634dd32b21005f5d
Epicjii <jacobliggett2000@gmail.com>
2022-04-02 21:45:27 -0400

    changing event priority

--- src/main/kotlin/chatoverhaul/minimessage/ChatFormatter.kt
+++ src/main/kotlin/chatoverhaul/minimessage/ChatFormatter.kt
@@ -4,12 +4,13 @@ import io.papermc.paper.event.player.AsyncChatEvent
 import net.kyori.adventure.text.TextComponent
 import net.kyori.adventure.text.minimessage.MiniMessage
 import org.bukkit.event.EventHandler
+import org.bukkit.event.EventPriority
 import org.bukkit.event.Listener
 
 class ChatFormatter : Listener {
     private val messageManipulator = MiniMessage.miniMessage()
 
-    @EventHandler
+    @EventHandler(priority = EventPriority.HIGH)
     fun reformatChatMessage(event: AsyncChatEvent) {
 
         val message = event.message() as TextComponent
