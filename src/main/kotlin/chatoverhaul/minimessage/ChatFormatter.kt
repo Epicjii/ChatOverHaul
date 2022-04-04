8ce60b47eec585d39965d631717b6fb139a10441
Epicjii <jacobliggett2000@gmail.com>
2022-04-04 18:00:13 -0400

    More Compatibility Updates

--- src/main/kotlin/chatoverhaul/minimessage/ChatFormatter.kt
+++ src/main/kotlin/chatoverhaul/minimessage/ChatFormatter.kt
@@ -2,6 +2,7 @@ package chatoverhaul.minimessage
 
 import io.papermc.paper.event.player.AsyncChatEvent
 import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.JoinConfiguration
 import net.kyori.adventure.text.TextComponent
 import net.kyori.adventure.text.minimessage.MiniMessage
 import org.bukkit.event.EventHandler
@@ -11,21 +12,40 @@ import org.bukkit.event.Listener
 class ChatFormatter : Listener {
     private val messageManipulator = MiniMessage.miniMessage()
 
-    @EventHandler(priority = EventPriority.LOW)
+    @EventHandler(priority = EventPriority.HIGHEST)
     fun reformatChatMessage(event: AsyncChatEvent) {
 
-        val listOfChildMessage = event.message().children()
-        val message: String = chatRebuilder(listOfChildMessage) + (event.message() as TextComponent).content()
+        val listOfChildMessages = event.message().children()
+        val gatewayMessage = mutableListOf<Component>()
+        val miniMessages = mutableListOf<Component>()
+
+        if (listOfChildMessages.isNotEmpty()) {
+            for (component: Component in listOfChildMessages) {
+                if (component.hasStyling()) {
+                    gatewayMessage.add(component)
+                } else {
+                    miniMessages.add(component)
+                }
+            }
+        } else {
+            miniMessages.add(event.message())
+        }
+
+        val message: String = miniMessages.joinToString("") {
+            (it as TextComponent).content()
+        }
 
         val newMessage = messageManipulator.deserialize(message)
 
-        event.message(newMessage)
+        event.message(Component.join(JoinConfiguration.noSeparators(),gatewayMessage + newMessage))
     }
 
     private fun chatRebuilder(componentList: List<Component>): String {
         var message = ""
         if (componentList.isNotEmpty()) {
             for (component: Component in componentList) {
+
+
                 message += chatRebuilder(component.children())
                 message += (component as TextComponent).content()
             }
