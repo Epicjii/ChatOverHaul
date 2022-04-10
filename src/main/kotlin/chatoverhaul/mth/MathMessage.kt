5170f18aa49c2bb8c8bc669e42590655f0cc59f7
Epicjii <jacobliggett2000@gmail.com>
2022-04-09 20:06:54 -0400

    added error handling

--- src/main/kotlin/chatoverhaul/mth/MathMessage.kt
+++ src/main/kotlin/chatoverhaul/mth/MathMessage.kt
@@ -2,6 +2,9 @@ package chatoverhaul.mth
 
 import chatoverhaul.ChatUtilities
 import chatoverhaul.ChatUtilities.getContent
+import com.notkamui.keval.KevalInvalidExpressionException
+import com.notkamui.keval.KevalInvalidSymbolException
+import com.notkamui.keval.KevalZeroDivisionException
 import com.notkamui.keval.keval
 import io.papermc.paper.event.player.AsyncChatEvent
 import net.kyori.adventure.text.Component
@@ -33,7 +36,16 @@ class MathMessage : Listener {
     }
 
     private fun calculator(match: MatchResult): String {
-        val result = match.groups[1]!!.value.keval()
+        val result : Double
+        try {
+            result = match.groups[1]!!.value.keval()
+        } catch (err: KevalZeroDivisionException) {
+            return "Error: Cannot Divide by Zero"
+        } catch (err: KevalInvalidSymbolException) {
+            return "Error: Invalid Symbol in Expression"
+        } catch (err: KevalInvalidExpressionException) {
+            return "Error: Expression is invalid"
+        }
         if (result - result.roundToInt() != 0.0) {
             return result.toString()
         }
